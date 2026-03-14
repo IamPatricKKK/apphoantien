4efe1404e5bc63125165ac00a20b17eb3622fefc
PhamXuanTruong <letruongle325@gmail.com>
2026-03-15 00:25:59 +0700

    fix error build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({
-  subsets: ["latin", "vietnamese"],
-  display: "swap",
-});
 
 export const metadata: Metadata = {
   title: "Hoantienvui.com",
@@ -19,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="vi">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
